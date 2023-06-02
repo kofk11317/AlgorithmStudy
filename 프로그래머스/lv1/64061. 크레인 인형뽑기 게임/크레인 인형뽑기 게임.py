ccf235f728fda5e190239a310ef4175dc3ccefb0
kofk11317 <64919144+kofk11317@users.noreply.github.com>
2023-06-03 02:32:57 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.26 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,21 +1,16 @@
+
 def solution(board, moves):
-    def minus(x):
-        return x-1
-    moves=list(map(minus, moves))#[0, 4, 2, 4, 0, 1, 0, 3]
-    # len(board)
-    cnt=0
-    stack=[]
-    for j in  moves:
-        if len(stack)>1 and stack[-1]==stack[-2]:
-            stack.pop()#사라지기
-            stack.pop()
-            cnt+=2
-        for i in range (len(board)):    
-            if board[i][j] !=0:
-                stack.append(board[i][j])#스택에 추가
-                board[i][j]=0#뺀거니꺼 0으로 변경
-                break
-    return cnt            
+    cols = list(map(lambda x: list(filter(lambda y: y > 0, x)), zip(*board)))
+    a, s = 0, [0]
+
+    for m in moves:
+        if len(cols[m - 1]) > 0:
+            if (d := cols[m - 1].pop(0)) == (l := s.pop()):
+                a += 2
+            else:
+                s.extend([l, d])
+
+    return a       
 
 # 약 같은 모양의 인형 두 개가 바구니에 연속해서 쌓이게 되면 두 인형은 터뜨려지면서 바구니에서 사라지게 됩니다
 # 크레인을 모두 작동시킨 후 터트려져 사라진 인형의 개수를 return 하도록 solution 함수를 완성해주세요.
