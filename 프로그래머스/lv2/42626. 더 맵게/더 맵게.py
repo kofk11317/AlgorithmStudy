fb7569f5ae3c0f4f879a6c49e73eb820b383483f
kofk11317 <64919144+kofk11317@users.noreply.github.com>
2023-06-29 00:25:39 +0900

    [level 2] Title: 더 맵게, Time: 1622.61 ms, Memory: 51.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -7,25 +7,13 @@ def solution(scoville, K):
     # 최소힙 구조로 변환
     heapify(scoville)
     while scoville[0] < K:
-        if len(scoville) < 2:
-            cnt = -1
+        if len(scoville) <2:
+            cnt=-1
             break
         new1 = heappop(scoville)
         new2 = heappop(scoville)
         heappush(scoville, new1 + (new2 * 2))
         cnt += 1
     return cnt    
-#     cnt=0
-#     scoville.sort()
-#     while True:
-#         scoville=deque(scoville)
-#         new1=scoville.popleft()
-#         new2=scoville.popleft()
-#         scoville.append(new1+(new2*2))
-#         scoville=list(scoville)
-#         scoville.sort()
-#         cnt+=1
-#         if scoville[0]>=K:#애초에 정렬이 되어있기 때문에 min함수 쓸 필요가 없음
-#             break
-#     return cnt        
+
 # # 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
