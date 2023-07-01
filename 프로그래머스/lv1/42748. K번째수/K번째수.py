b73dae9a9986b834e65fe6e141c3db26db306e88
kofk11317 <64919144+kofk11317@users.noreply.github.com>
2023-07-01 23:24:07 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,11 +1,3 @@
 def solution(array, commands):
-    answer=[]
-    for i in (commands):
-            (start)=(i[0]-1)
-            (end)=i[1]
-            (select)=(i[2]-1)
-            new=array[start:end]
-            new.sort()
-
-            answer.append(new[select])
-    return answer        
+    answer = []
+    return answer
